141ecddd08f708ab0d7865b62a26e644119f2274
root <1609760554@qq.com>
2019-05-30 15:30:28 +0800

    发布项目

---
+++ 汉诺塔游戏/Disc.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+public class Disc extends JButton{
+   int number;
+   TowerPoint point;
+   Disc(){
+      setBackground(Color.ORANGE);
+   }                 
+   public void setNumber(int n){
+       number=n;
+   }
+   public int getNumber(){
+       return number;
+   }
+   public void setPoint(TowerPoint p){
+      point=p;
+   }
+   public TowerPoint getPoint(){
+      return point;
+   }
+}
